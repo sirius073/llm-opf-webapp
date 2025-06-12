133a888149653477411664ea4e9b9aeff5b7363e
sirius073 <149000848+sirius073@users.noreply.github.com>
2025-06-12 10:07:18 +0530

    Update model.py

--- core/model.py
+++ core/model.py
@@ -1,6 +1,6 @@
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
-from langchain.llms import HuggingFacePipeline
+from langchain_community.llms import HuggingFacePipeline
 
 def load_model(model_id: str):
     tokenizer = AutoTokenizer.from_pretrained(model_id)
@@ -11,5 +11,5 @@ def load_model(model_id: str):
         device_map="auto",
         attn_implementation="eager" 
     )
-    hf_pipeline = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=500, device=0)
+    hf_pipeline = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=500)
     return HuggingFacePipeline(pipeline=hf_pipeline)
