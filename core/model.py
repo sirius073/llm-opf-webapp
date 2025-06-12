d28c3ceae191513270cfb5b93606507bb081c703
sirius073 <149000848+sirius073@users.noreply.github.com>
2025-06-12 10:55:09 +0530

    Update model.py

--- core/model.py
+++ core/model.py
@@ -9,7 +9,8 @@ def load_model(model_id: str):
         model_id,
         torch_dtype=torch.float16 if device == "cuda" else torch.float32,
         device_map="auto",
-        attn_implementation="eager" 
+        attn_implementation="eager",
+        trust_remote_code=True 
     )
     hf_pipeline = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=500)
     return HuggingFacePipeline(pipeline=hf_pipeline)
